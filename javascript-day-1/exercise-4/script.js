893669ccd931eb4dfe0a204ccd096a92fa6ca6cb
Piyush Tripathi <piyushtripathi95@gmail.com>
2026-05-09 22:11:46 +0530

    day-1 exercise-4

---
+++ javascript-day-1/exercise-4/script.js
@@ -0,0 +1,13 @@
+const string = "string";
+const number = 32;
+let isBoolean = true;
+let x;
+const object = {
+    name:"Piyush",
+    age:32
+}
+console.log(string,typeof string);
+console.log(number,typeof number);
+console.log(isBoolean,typeof isBoolean);
+console.log(x,typeof x);
+console.log(object,typeof object);
