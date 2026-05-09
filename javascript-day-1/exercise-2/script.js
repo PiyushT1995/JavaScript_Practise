91d60096ab3cd30416b1ddbf785a692d467d2ef6
Piyush Tripathi <piyushtripathi95@gmail.com>
2026-05-09 20:43:28 +0530

    day-1 exercise-2

---
+++ javascript-day-1/exercise-2/script.js
@@ -0,0 +1,9 @@
+let name = "Piyush";
+const age  = 32;
+let city = "Bengaluru";
+const isWorking = true;
+console.log(name);
+console.log(age);
+console.log(city);
+console.log(isWorking);
+console.log("My name is "+name+" and I live in "+city);
