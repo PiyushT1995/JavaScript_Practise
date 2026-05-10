2b2263c3eab723d614de0b8c73d24074f95a6168
Piyush Tripathi <piyushtripathi95@gmail.com>
2026-05-10 12:38:35 +0530

    day-1 exercise-9

---
+++ javascript-day-1/exercise-9/script.js
@@ -0,0 +1,13 @@
+let x = null;
+console.log(x,typeof x);
+
+let y;
+console.log(y,typeof y);
+
+const user = {
+  name: "Piyush",
+  email: "piyush@gmail.com",
+  phone: null
+};
+
+console.log(user);
