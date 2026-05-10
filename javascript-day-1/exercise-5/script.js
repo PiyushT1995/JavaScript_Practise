83d35bff6741feb2ad484a4ef9a7f5790e233b91
Piyush Tripathi <piyushtripathi95@gmail.com>
2026-05-10 11:44:19 +0530

    day-1 excercise-5

---
+++ javascript-day-1/exercise-5/script.js
@@ -0,0 +1,20 @@
+const student =  {
+    name : "Piyush",
+    age : 32,
+    course : "JS",
+    isEnrolled : true,
+    city : "Bhopal",
+    skills : "SDE2",
+    experience : 3.7
+
+
+}
+console.log(student);
+console.log(student.name);
+console.log(student.age);
+console.log(student.course);
+console.log(student.isEnrolled);
+console.log(student.city);
+console.log(student.skills);
+console.log(student.experience);
+console.log(`${student.name} is enrolled in ${student.course}`);
