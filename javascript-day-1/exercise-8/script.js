542149312babf1058f19a98b3c5808a7cc100dc3
Piyush Tripathi <piyushtripathi95@gmail.com>
2026-05-10 12:29:32 +0530

    day-1 exercise-8

---
+++ javascript-day-1/exercise-8/script.js
@@ -0,0 +1,17 @@
+const profile = {
+    name : "Piyush",
+    age : 32,
+    city : "Bhopal",
+    profession : "SDE2",
+    hobby : "games"
+}
+console.log(profile);
+console.log(profile.name);
+console.log(profile.age);
+console.log(profile.city);
+console.log(profile.profession);
+console.log(profile.hobby);
+console.log(`My name is ${profile.name}`);
+console.log(`I live in ${profile.city}`);
+console.log(`My profession is ${profile.profession}`);
+console.log(`My hobby is ${profile.hobby}`);
