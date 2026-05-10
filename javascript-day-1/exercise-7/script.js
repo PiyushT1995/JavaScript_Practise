8f000455460bdfbe4d5abc2b6251e404e5ba69c3
Piyush Tripathi <piyushtripathi95@gmail.com>
2026-05-10 12:17:36 +0530

    day-1 exercise-7

---
+++ javascript-day-1/exercise-7/script.js
@@ -0,0 +1,18 @@
+let city = "Bhopal";
+console.log(city);
+city = "Bengaluru";
+console.log(city);
+const pi = 3.15;
+console.log(pi);
+const car = {
+    name : "Maruti",
+    price : 123
+}
+console.log(car);
+console.log(car.name);
+console.log(car.price);
+
+car.price = 456;
+console.log(car);
+console.log(car.name);
+console.log(car.price);
