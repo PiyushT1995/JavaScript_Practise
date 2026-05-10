1ed6320a68d951420ed809419d432927234f284e
Piyush Tripathi <piyushtripathi95@gmail.com>
2026-05-10 15:33:31 +0530

    day-2 exercise-5

---
+++ javascript-day-2/exercise-5/script.js
@@ -0,0 +1,10 @@
+let marks = 71;
+if(marks>=90){
+    console.log("A");
+}else if(marks>=70){
+    console.log("B");
+}else if(marks>=50){
+    console.log("C");
+}else{
+    console.log("Fail");
+}
