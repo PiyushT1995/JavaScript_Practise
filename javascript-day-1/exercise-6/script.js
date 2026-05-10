e377b2eb54bfde161705890b1985d774d74e8f6e
Piyush Tripathi <piyushtripathi95@gmail.com>
2026-05-10 11:58:33 +0530

    day-1 exercise-6

---
+++ javascript-day-1/exercise-6/script.js
@@ -0,0 +1,12 @@
+const car = {
+    brand : "Maruti",
+    model : "M1982",
+    year : 1982,
+    color : "red"
+}
+console.log(car);
+console.log(car.brand);
+console.log(car.model);
+console.log(car.year);
+console.log(car.color);
+console.log(`${car.brand} ${car.model} is ${car.color} and released in ${car.year}`);
